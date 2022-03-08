45efdf69874cd902b31fbe897879e6bb24829a90
rgraue <rgraue@outlook.com>
2022-03-08 11:11:21 -0800

    xhhtp request property

--- public/scripts/retrieveProperty.js
+++ public/scripts/retrieveProperty.js
@@ -6,8 +6,10 @@ function getProperty () {
                 console.log(this.responseText);
             }
         }
-
+        body = JSON.stringify({id : params.id});
         xhttp.open('POST', 'localhost:8080/api/property') //BUGBUG end point not created and only works for local env.
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.send(body);
         // send post request
     } else {
         errorRedirect();
