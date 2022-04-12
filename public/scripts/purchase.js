83f6169030f17a5b3175dc76ac8bef5a3f787b04
rgraue <rgraue@outlook.com>
2022-04-12 10:47:46 -0700

    chainID check

--- public/scripts/purchase.js
+++ public/scripts/purchase.js
@@ -1,5 +1,24 @@
 
 
+setup();
+
+async function setup () {
+    if (checkMM()) {
+        ethereum.request({method : 'eth_chainId'})
+        .then((result) => {
+            if (result === '0x3') {
+                console.log("hit");
+                // Set correct chain fox
+                $('#metaCheck').html('<img src="./icon/fox.png" height="50" width="50">');
+            } else {
+                // set chain
+            }
+        })
+    } else {
+        // download metamask
+    }
+}
+
 function checkMM () {
     if (!window.ethereum){  // needs to install/connect metamask
         console.log("Metamask is not installed");
