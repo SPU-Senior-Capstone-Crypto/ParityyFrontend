eb8958ec76f856083ad30c45f13920098a32e244
rgraue <rgraue@outlook.com>
2022-05-26 22:34:25 -0700

    big clean

--- public/images/Parityy_files/urlParams.js
+++
@@ -1,5 +0,0 @@
-const urlParameters = new URLSearchParams(window.location.search);
-const params = {};
-urlParameters.forEach((val, key) => {
-    params[key] = val;
-});
---
+++ public/scripts/index.js
@@ -0,0 +1,26 @@
+// Team carousel
+$(".team-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    dots: false,
+    loop: true,
+    nav : true,
+    navText : [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+});
