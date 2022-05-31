267a82c638513da68ebdb3b138a4d4199d7c3c96
Issa Mathno <imathno@gmail.com>
2022-05-30 23:08:47 -0700

    Removed the sessions stuff from footer

--- public/scripts/footer.js
+++ public/scripts/footer.js
@@ -1,24 +1,3 @@
-
 $(function(){
-    $("#footer-placeholder").load("/components/footer.html", () => {
-        if (getSSID() > 0){
-            $('#accountBtn').html(
-                '<a href="profile.html">Account</a>'
-                );
-            $('#accountOptionBtn').html(
-                'Logout'
-            ).css({
-                'background-color': 'black',
-                'border-color' : 'black',
-                'color' : 'lightgrey'
-            }).on('click', () => {
-                deleteCookie();
-            });
-            $('#propBtn').css(
-                {
-                    'display':'grid'
-                }
-            ); 
-        }
-    });
-});
+    $("#nav-placeholder").load("/components/nav.html");
+}); 
